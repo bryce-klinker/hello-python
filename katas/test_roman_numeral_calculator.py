322e85fc44206e80f33bac419481ff3af4672e68
bryce-klinker <bklinker@pillartechnology.com>
2016-08-29 15:31:16 -0500

    Moved creation of calculator into setup method

--- katas/test_roman_numeral_calculator.py
+++ katas/test_roman_numeral_calculator.py
@@ -3,19 +3,19 @@ from roman_numeral_calculator import RomanNumeralCalculator
 
 
 class RomanNumeralCalculatorTest(unittest.TestCase):
+    def setUp(self):
+        self.calculator = RomanNumeralCalculator()
+
     def test_I_plus_I_equals_II(self):
-        calculator = RomanNumeralCalculator()
-        result = calculator.add('I', 'I')
+        result = self.calculator.add('I', 'I')
         self.assertEqual('II', result)
 
     def test_II_plus_I_equals_III(self):
-        calculator = RomanNumeralCalculator()
-        result = calculator.add('II', 'I')
+        result = self.calculator.add('II', 'I')
         self.assertEqual('III', result)
 
     def test_I_plus_II_equals_III(self):
-        calculator = RomanNumeralCalculator()
-        result = calculator.add('I', 'II')
+        result = self.calculator.add('I', 'II')
         self.assertEqual('III', result)
 
 if __name__ == '__main__':
