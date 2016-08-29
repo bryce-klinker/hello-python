0c314085749f50db653b6660cf1a5c277c45386c
bryce-klinker <bklinker@pillartechnology.com>
2016-08-29 16:01:24 -0500

    Removed to_arabic from roman_numeral_converter

--- katas/roman_numeral_converter.py
+++ katas/roman_numeral_converter.py
@@ -1,21 +1,4 @@
 class RomanNumeralConverter(object):
-    def to_arabic(self, roman):
-        if roman == 'X':
-            return 10
-
-        if roman == 'IX':
-            return 9
-
-        if roman == 'IV':
-            return 4
-
-        if roman == 'III':
-            return 3
-
-        if roman == 'II':
-            return 2
-        return 1
-
     def to_roman(self, arabic):
         if arabic == 2:
             return 'II'
