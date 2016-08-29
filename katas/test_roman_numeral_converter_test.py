fc19ca460efd09a2d36743b454b095b158491aa6
bryce-klinker <bklinker@pillartechnology.com>
2016-08-29 16:02:42 -0500

    Moved tests for converting to arabic into new test file

--- katas/test_roman_numeral_converter_test.py
+++ katas/test_roman_numeral_converter_test.py
@@ -1,36 +1,11 @@
 import unittest
 
-from katas.roman_to_arabic_converter import to_arabic
 from katas.roman_numeral_converter import RomanNumeralConverter
 
 
 class RomanNumeralConverterTest(unittest.TestCase):
     def setUp(self):
         self.converter = RomanNumeralConverter()
-
-    def test_I_to_arabic_equals_1(self):
-        result = to_arabic('I')
-        self.assertEquals(1, result)
-
-    def test_II_to_arabic_equals_2(self):
-        result = to_arabic('II')
-        self.assertEquals(2, result)
-
-    def test_III_to_arabic_equals_3(self):
-        result = to_arabic('III')
-        self.assertEquals(3, result)
-
-    def test_IV_to_arabic_equals_4(self):
-        result = to_arabic('IV')
-        self.assertEquals(4, result)
-
-    def test_IX_to_arabic_equals_9(self):
-        result = to_arabic('IX')
-        self.assertEquals(9, result)
-
-    def test_X_to_arabic_equals_10(self):
-        result = to_arabic('X')
-        self.assertEquals(10, result)
 
     def test_1_to_roman_equals_I(self):
         result = self.converter.to_roman(1)
