e3949d670897e83f94ec2fefcdbbc3dc33851baa
bryce-klinker <bklinker@pillartechnology.com>
2016-08-29 15:25:08 -0500

    Moved files into katas folder

--- katas/test_roman_numeral_calculator.py
+++ katas/test_roman_numeral_calculator.py
@@ -1,11 +1,17 @@
 import unittest
 from roman_numeral_calculator import RomanNumeralCalculator
 
+
 class RomanNumeralCalculatorTest(unittest.TestCase):
     def test_I_plus_I_equals_II(self):
         calculator = RomanNumeralCalculator()
         result = calculator.add('I', 'I')
         self.assertEqual('II', result)
+
+    def test_II_plus_I_equals_III(self):
+        calculator = RomanNumeralCalculator()
+        result = calculator.add('II', 'I')
+        self.assertEqual('III', result)
 
 
 if __name__ == '__main__':
