349f14e3f5cbd8f919d4c7c28537689d43d16bbd
bryce-klinker <bklinker@pillartechnology.com>
2016-08-29 15:49:58 -0500

    Refactored creation of converter into setup method

--- katas/test_roman_numeral_converter_test.py
+++ katas/test_roman_numeral_converter_test.py
@@ -4,19 +4,19 @@ from katas.roman_numeral_converter import RomanNumeralConverter
 
 
 class RomanNumeralConverterTest(unittest.TestCase):
+    def setUp(self):
+        self.converter = RomanNumeralConverter()
+
     def test_I_to_arabic_equals_1(self):
-        converter = RomanNumeralConverter()
-        result = converter.to_arabic('I')
+        result = self.converter.to_arabic('I')
         self.assertEquals(1, result)
 
     def test_II_to_arabic_equals_2(self):
-        converter = RomanNumeralConverter()
-        result = converter.to_arabic('II')
+        result = self.converter.to_arabic('II')
         self.assertEquals(2, result)
 
     def test_III_to_arabic_equals_3(self):
-        converter = RomanNumeralConverter()
-        result = converter.to_arabic('III')
+        result = self.converter.to_arabic('III')
         self.assertEquals(3, result)
 
 if __name__ == '__main__':
