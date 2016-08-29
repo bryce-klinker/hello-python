6c671df09cbef2b7bb3b7b819085641720918244
bryce-klinker <bklinker@pillartechnology.com>
2016-08-29 15:29:38 -0500

    Fixed test I + II = III

--- katas/roman_numeral_calculator.py
+++ katas/roman_numeral_calculator.py
@@ -1,5 +1,5 @@
 class RomanNumeralCalculator(object):
     def add(self, first, second):
-        if first == 'II':
+        if first == 'II' or second == 'II':
             return 'III'
         return 'II'
