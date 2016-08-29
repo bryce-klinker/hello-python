e84eedf1dc57965e80f1154f6657a4a1ccecf2ca
bryce-klinker <bklinker@pillartechnology.com>
2016-08-29 15:28:33 -0500

    Added test for reverse of last

--- katas/test_roman_numeral_calculator.py
+++ katas/test_roman_numeral_calculator.py
@@ -13,6 +13,10 @@ class RomanNumeralCalculatorTest(unittest.TestCase):
         result = calculator.add('II', 'I')
         self.assertEqual('III', result)
 
+    def test_I_plus_II_equals_III(self):
+        calculator = RomanNumeralCalculator()
+        result = calculator.add('I', 'II')
+        self.assertEqual('III', result)
 
 if __name__ == '__main__':
     unittest.main()
