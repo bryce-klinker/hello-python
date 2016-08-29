d0f36ff14956994a61dfd7c34aab343497d18dba
bryce-klinker <bklinker@pillartechnology.com>
2016-08-29 15:57:46 -0500

    Added 2 to roman equals II test

--- katas/test_roman_numeral_converter_test.py
+++ katas/test_roman_numeral_converter_test.py
@@ -35,5 +35,9 @@ class RomanNumeralConverterTest(unittest.TestCase):
         result = self.converter.to_roman(1)
         self.assertEquals('I', result)
 
+    def test_2_to_roman_equals_II(self):
+        result = self.converter.to_roman(2)
+        self.assertEquals('II', result)
+
 if __name__ == '__main__':
     unittest.main()
