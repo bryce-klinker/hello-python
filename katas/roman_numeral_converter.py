8002cf7385dd14c635ce8be01ba830a2e38ab596
bryce-klinker <bklinker@pillartechnology.com>
2016-08-29 15:49:05 -0500

    Fixed III to arabic equals 3 test

--- katas/roman_numeral_converter.py
+++ katas/roman_numeral_converter.py
@@ -1,5 +1,8 @@
 class RomanNumeralConverter(object):
     def to_arabic(self, roman):
+        if roman == 'III':
+            return 3
+        
         if roman == 'II':
             return 2
         return 1
