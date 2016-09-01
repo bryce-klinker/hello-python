1b1b859cc9cc2b796b4229b1310bee26f1c38fb5
bryce-klinker <bklinker@pillartechnology.com>
2016-09-01 09:58:02 -0500

    Updated calculator tests to use common helper method

--- katas/test_roman_numeral_calculator.py
+++ katas/test_roman_numeral_calculator.py
@@ -7,36 +7,32 @@ class RomanNumeralCalculatorTest(unittest.TestCase):
         self.calculator = RomanNumeralCalculator()
 
     def test_I_plus_I_equals_II(self):
-        result = self.calculator.add('I', 'I')
-        self.assertEqual('II', result)
+        self.assert_add_equals("I", "I", "II")
 
     def test_II_plus_I_equals_III(self):
-        result = self.calculator.add('II', 'I')
-        self.assertEqual('III', result)
+        self.assert_add_equals("II", "I", "III")
 
     def test_I_plus_II_equals_III(self):
-        result = self.calculator.add('I', 'II')
-        self.assertEqual('III', result)
+        self.assert_add_equals("I", "II", "III")
 
     def test_II_plus_II_equals_IV(self):
-        result = self.calculator.add('II', 'II')
-        self.assertEquals('IV', result)
+        self.assert_add_equals("II", "II", "IV")
 
     def test_IV_plus_I_equals_V(self):
-        result = self.calculator.add('IV', 'I')
-        self.assertEquals('V', result)
+        self.assert_add_equals("IV", "I", "V")
 
     def test_I_plus_IV_equals_V(self):
-        result = self.calculator.add('I', 'IV')
-        self.assertEquals('V', result)
+        self.assert_add_equals("I", "IV", "V")
 
     def test_V_plus_V_equals_X(self):
-        result = self.calculator.add('V', 'V')
-        self.assertEquals('X', result)
+        self.assert_add_equals("V", "V", "X")
 
     def test_IX_plus_I_equals_X(self):
-        result = self.calculator.add('IX', 'I')
-        self.assertEquals('X', result)
+        self.assert_add_equals("IX", "I", "X")
+
+    def assert_add_equals(self, first, second, expected):
+        actual = self.calculator.add(first, second)
+        self.assertEquals(expected, actual)
 
 if __name__ == '__main__':
     unittest.main()
