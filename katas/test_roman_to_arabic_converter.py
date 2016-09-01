4dcaf284f1dc3ee5b1602efa621b4386c9f9f627
bryce-klinker <bklinker@pillartechnology.com>
2016-09-01 09:41:07 -0500

    Changed roman to arabic tests to use RomanNumeral.from_roman

--- katas/test_roman_to_arabic_converter.py
+++ katas/test_roman_to_arabic_converter.py
@@ -1,32 +1,33 @@
 import unittest
 
-from katas.roman_to_arabic_converter import roman_to_arabic
+from katas.roman_numeral import RomanNumeral
+
 
 class RomanToArabicConverterTest(unittest.TestCase):
     def test_I_to_arabic_equals_1(self):
-        result = roman_to_arabic('I')
-        self.assertEquals(1, result)
+        result = RomanNumeral.from_roman('I')
+        self.assertEquals(1, result.arabic)
 
     def test_II_to_arabic_equals_2(self):
-        result = roman_to_arabic('II')
-        self.assertEquals(2, result)
+        result = RomanNumeral.from_roman('II')
+        self.assertEquals(2, result.arabic)
 
     def test_III_to_arabic_equals_3(self):
-        result = roman_to_arabic('III')
-        self.assertEquals(3, result)
+        result = RomanNumeral.from_roman('III')
+        self.assertEquals(3, result.arabic)
 
     def test_IV_to_arabic_equals_4(self):
-        result = roman_to_arabic('IV')
-        self.assertEquals(4, result)
+        result = RomanNumeral.from_roman('IV')
+        self.assertEquals(4, result.arabic)
 
     def test_V_to_arabic_equals_5(self):
-        result = roman_to_arabic('V')
-        self.assertEquals(5, result)
+        result = RomanNumeral.from_roman('V')
+        self.assertEquals(5, result.arabic)
 
     def test_IX_to_arabic_equals_9(self):
-        result = roman_to_arabic('IX')
-        self.assertEquals(9, result)
+        result = RomanNumeral.from_roman('IX')
+        self.assertEquals(9, result.arabic)
 
     def test_X_to_arabic_equals_10(self):
-        result = roman_to_arabic('X')
-        self.assertEquals(10, result)
+        result = RomanNumeral.from_roman('X')
+        self.assertEquals(10, result.arabic)
