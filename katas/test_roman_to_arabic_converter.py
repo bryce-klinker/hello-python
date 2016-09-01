aac699c9064f40370d3710d4b983f3b8c159f188
bryce-klinker <bklinker@pillartechnology.com>
2016-09-01 09:54:52 -0500

    Updated roman to arabic to use common helper method

--- katas/test_roman_to_arabic_converter.py
+++ katas/test_roman_to_arabic_converter.py
@@ -5,29 +5,26 @@ from katas.roman_numeral import RomanNumeral
 
 class RomanToArabicConverterTest(unittest.TestCase):
     def test_I_to_arabic_equals_1(self):
-        result = RomanNumeral.from_roman('I')
-        self.assertEquals(1, result.arabic)
+        self.assert_roman_to_arabic("I", 1)
 
     def test_II_to_arabic_equals_2(self):
-        result = RomanNumeral.from_roman('II')
-        self.assertEquals(2, result.arabic)
+        self.assert_roman_to_arabic("II", 2)
 
     def test_III_to_arabic_equals_3(self):
-        result = RomanNumeral.from_roman('III')
-        self.assertEquals(3, result.arabic)
+        self.assert_roman_to_arabic("III", 3)
 
     def test_IV_to_arabic_equals_4(self):
-        result = RomanNumeral.from_roman('IV')
-        self.assertEquals(4, result.arabic)
+        self.assert_roman_to_arabic("IV", 4)
 
     def test_V_to_arabic_equals_5(self):
-        result = RomanNumeral.from_roman('V')
-        self.assertEquals(5, result.arabic)
+        self.assert_roman_to_arabic("V", 5)
 
     def test_IX_to_arabic_equals_9(self):
-        result = RomanNumeral.from_roman('IX')
-        self.assertEquals(9, result.arabic)
+        self.assert_roman_to_arabic("IX", 9)
 
     def test_X_to_arabic_equals_10(self):
-        result = RomanNumeral.from_roman('X')
-        self.assertEquals(10, result.arabic)
+        self.assert_roman_to_arabic("X", 10)
+
+    def assert_roman_to_arabic(self, roman, arabic):
+        result = RomanNumeral.from_roman(roman)
+        self.assertEquals(arabic, result.arabic)
