28a66784503dab6be3167d9627fb887e9a8ff837
bryce-klinker <bklinker@pillartechnology.com>
2016-09-01 09:28:35 -0500

    Moving conversion to and from roman numerals to RomanNumeral class

--- katas/roman_numeral.py
+++ katas/roman_numeral.py
@@ -1,8 +1,5 @@
-from katas.roman_to_arabic_converter import roman_to_arabic
-from katas.arabic_to_roman_converter import arabic_to_roman
-
-
 class RomanNumeral:
+
     def __init__(self, roman):
         self._roman = roman
 
@@ -12,9 +9,58 @@ class RomanNumeral:
 
     @property
     def arabic(self):
-        return roman_to_arabic(self._roman)
+        return RomanNumeral.convert_to_arabic(self.roman)
 
     def add(self, roman_numeral):
         arabic_sum = self.arabic + roman_numeral.arabic
-        roman_sum = arabic_to_roman(arabic_sum)
-        return RomanNumeral(roman_sum)
+        return RomanNumeral.from_arabic(arabic_sum)
+
+    @staticmethod
+    def get_map():
+        return {
+            "I": 1,
+            "IV": 4,
+            "V": 5,
+            "IX": 9,
+            "X": 10
+        }
+
+    @staticmethod
+    def from_arabic(arabic):
+        roman_to_arabic_map = RomanNumeral.get_map()
+        roman = ""
+        remainder = arabic
+        while remainder > 0:
+            for key, value in sorted(roman_to_arabic_map.items(), reverse=True):
+                if remainder >= value:
+                    remainder -= value
+                    roman += key
+                    break
+
+        return RomanNumeral(roman)
+
+    @staticmethod
+    def from_roman(roman):
+        return RomanNumeral(roman)
+
+    @staticmethod
+    def convert_to_arabic(roman):
+        roman_to_arabic_map = RomanNumeral.get_map()
+        value = 0
+        index = 0
+        length = len(roman)
+        while index < length:
+            first = roman[index]
+            second = ''
+            if index < length - 1:
+                second = roman[index + 1]
+
+            if first + second in roman_to_arabic_map.keys():
+                value += roman_to_arabic_map[first + second]
+                index += 1
+            else:
+                value += roman_to_arabic_map[first]
+
+            index += 1
+
+        return value
