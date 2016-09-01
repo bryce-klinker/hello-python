8b28bd04c12894f4fcf5a7fdba5c4806e50a9c36
bryce-klinker <bklinker@pillartechnology.com>
2016-09-01 08:38:37 -0500

    Changed arabic to roman to use a hash instead of if statements

--- katas/arabic_to_roman_converter.py
+++ katas/arabic_to_roman_converter.py
@@ -1,19 +1,20 @@
+arabic_to_roman_map = {
+    10: "X",
+    9: "IX",
+    5: "V",
+    4: "IV",
+    1: "I"
+}
+
+
 def arabic_to_roman(arabic):
-    if arabic == 10:
-        return 'X'
+    roman = ""
+    remainder = arabic
+    while remainder > 0:
+        for key in sorted(arabic_to_roman_map.keys(), reverse=True):
+            if remainder >= key:
+                remainder -= key
+                roman += arabic_to_roman_map[key]
+                break
 
-    if arabic == 9:
-        return 'IX'
-
-    if arabic == 5:
-        return 'V'
-
-    if arabic == 4:
-        return 'IV'
-
-    if arabic == 3:
-        return 'III'
-
-    if arabic == 2:
-        return 'II'
-    return 'I'
+    return roman
