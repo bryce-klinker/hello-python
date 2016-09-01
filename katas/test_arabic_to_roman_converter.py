531152c5c7fe5e10d67a77e14b139ebf001a47c1
bryce-klinker <bklinker@pillartechnology.com>
2016-09-01 09:51:26 -0500

    Updated arabic to roman tests to use a common helper method

--- katas/test_arabic_to_roman_converter.py
+++ katas/test_arabic_to_roman_converter.py
@@ -5,44 +5,38 @@ from katas.roman_numeral import RomanNumeral
 
 class ArabicToRomanConverterTest(unittest.TestCase):
     def test_1_to_roman_equals_I(self):
-        result = RomanNumeral.from_arabic(1)
-        self.assertEquals('I', result.roman)
+        self.assert_arabic_to_roman(1, "I")
 
     def test_2_to_roman_equals_II(self):
-        result = RomanNumeral.from_arabic(2)
-        self.assertEquals('II', result.roman)
+        self.assert_arabic_to_roman(2, "II")
 
     def test_3_to_roman_equals_III(self):
-        result = RomanNumeral.from_arabic(3)
-        self.assertEquals('III', result.roman)
+        self.assert_arabic_to_roman(3, "III")
 
     def test_4_to_roman_equals_IV(self):
-        result = RomanNumeral.from_arabic(4)
-        self.assertEquals('IV', result.roman)
+        self.assert_arabic_to_roman(4, "IV")
 
     def test_5_to_roman_equals_V(self):
-        result = RomanNumeral.from_arabic(5)
-        self.assertEquals('V', result.roman)
+        self.assert_arabic_to_roman(5, "V")
 
     def test_9_to_roman_equals_IX(self):
-        result = RomanNumeral.from_arabic(9)
-        self.assertEquals('IX', result.roman)
+        self.assert_arabic_to_roman(9, "IX")
 
     def test_10_to_roman_equals_X(self):
-        result = RomanNumeral.from_arabic(10)
-        self.assertEquals('X', result.roman)
+        self.assert_arabic_to_roman(10, "X")
 
     def test_20_to_roman_equals_XX(self):
-        result = RomanNumeral.from_arabic(20)
-        self.assertEquals('XX', result.roman)
+        self.assert_arabic_to_roman(20, "XX")
 
     def test_40_to_roman_equals_XL(self):
-        result = RomanNumeral.from_arabic(40)
-        self.assertEquals("XL", result.roman)
+        self.assert_arabic_to_roman(40, "XL")
 
     def test_50_to_roman_equals_L(self):
-        result = RomanNumeral.from_arabic(50)
-        self.assertEquals("L", result.roman)
+        self.assert_arabic_to_roman(50, "L")
+
+    def assert_arabic_to_roman(self, arabic, roman):
+        result = RomanNumeral.from_arabic(arabic)
+        self.assertEquals(roman, result.roman)
 
 if __name__ == '__main__':
     unittest.main()
