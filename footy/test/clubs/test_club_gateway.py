d9c03fda80d4a37f139e3b1e2b910703f79d7b34
bryce-klinker <bklinker@pillartechnology.com>
2016-09-02 10:29:20 -0500

    Added test for ensuring get all clubs gets club names

--- footy/test/clubs/test_club_gateway.py
+++ footy/test/clubs/test_club_gateway.py
@@ -8,3 +8,10 @@ class ClubGatewayTest(unittest.TestCase):
         gateway = ClubGateway()
         clubs = gateway.get_all()
         self.assertEquals(20, len(clubs))
+
+    def test_get_all_clubs_includes_club_name(self):
+        gateway = ClubGateway()
+        clubs = gateway.get_all()
+        self.assertEquals("Arsenal", clubs[0].club_name)
+        self.assertEquals("Aston Villa", clubs[1].club_name)
+        self.assertEquals("Bournemouth", clubs[2].club_name)
